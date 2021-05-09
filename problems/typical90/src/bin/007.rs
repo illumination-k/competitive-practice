c0ed6102f329adca603940a8ca9c8889254fe1ae
illumination-k <illumination.k.27@gmail.com>
2021-05-10 02:01:51 +0900

    autocommit on submission (./src/bin/007.rs)

--- problems/typical90/src/bin/007.rs
+++ problems/typical90/src/bin/007.rs
@@ -2,20 +2,20 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use proconio::{input, fastout};
-use proconio::marker::*;
-use whiteread::parse_line;
-use std::collections::*;
 use num::*;
 use num_traits::*;
-use superslice::*;
+use proconio::marker::*;
+use proconio::{fastout, input};
+use std::collections::*;
 use std::ops::*;
+use superslice::*;
+use whiteread::parse_line;
 
-use itertools::Itertools;
 use itertools::iproduct;
+use itertools::Itertools;
 use itertools_num::ItertoolsNum;
 
-use competitive::format::*;
+use competitive_internal_mod::format::*;
 
 #[allow(unused_macros)]
 macro_rules! debug {
@@ -31,11 +31,120 @@ fn solve() -> impl AtCoderFormat {
     const UINF: usize = std::usize::MAX;
     const IINF: isize = std::isize::MAX;
 
-    input!{}
-    
-    ""
+    input! {
+        n: usize,
+        mut a: [isize; n],
+        q: usize,
+        b: [isize; q]
+    }
+
+    /*
+    自分に一番近いところのクラスを探す
+    二分探索すればO(qlogn)
+    */
+
+    a.sort();
+    let mut ans = vec![];
+    for x in b.iter() {
+        let idx = a.lower_bound(x);
+        let t = if idx == 0 {
+            (*x - a[idx]).abs()
+        } else if idx == a.len() {
+            (*x - a[idx - 1]).abs()
+        } else {
+            std::cmp::min((*x - a[idx - 1]).abs(), (*x - a[idx]).abs())
+        };
+
+        ans.push(t);
+    }
+    ans
 }
 
 fn main() {
     println!("{}", solve().format());
+}
+
+mod competitive_internal_mod {
+    pub mod format {
+        use std::vec::Vec;
+
+        /// Trait of format for atcoder
+        ///    
+        /// bool -> Yes or No  
+        /// vec![a, b ,c] -> "a\nb\nc"  
+        /// vec![vec![0, 1], vec![1, 0]] -> "0 1\n1 0"  
+        pub trait AtCoderFormat {
+            fn format(&self) -> String;
+        }
+
+        macro_rules! impl_format {
+            ($t: ty) => {
+                impl AtCoderFormat for $t {
+                    fn format(&self) -> String {
+                        self.to_string()
+                    }
+                }
+
+                impl AtCoderFormat for Vec<$t> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+
+                impl AtCoderFormat for Vec<Vec<$t>> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| {
+                                x.iter()
+                                    .map(|x| x.to_string())
+                                    .collect::<Vec<String>>()
+                                    .join(" ")
+                            })
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+            };
+        }
+
+        impl_format!(usize);
+        impl_format!(u128);
+        impl_format!(u64);
+        impl_format!(u32);
+        impl_format!(u16);
+        impl_format!(u8);
+        impl_format!(isize);
+        impl_format!(i128);
+        impl_format!(i64);
+        impl_format!(i32);
+        impl_format!(i16);
+        impl_format!(i8);
+        impl_format!(f32);
+        impl_format!(f64);
+        impl_format!(&str);
+        impl_format!(String);
+        impl_format!(char);
+
+        impl AtCoderFormat for bool {
+            fn format(&self) -> String {
+                if self == &true {
+                    "Yes".to_string()
+                } else {
+                    "No".to_string()
+                }
+            }
+        }
+
+        impl AtCoderFormat for Vec<bool> {
+            fn format(&self) -> String {
+                self.iter()
+                    .map(|x| x.format())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            }
+        }
+    }
 }
