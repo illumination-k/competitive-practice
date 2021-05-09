46de196564a91d90ea4dfc8e1b26f338a1f2fc7c
illumination-k <illumination.k.27@gmail.com>
2021-05-10 00:20:51 +0900

    autocommit on submission (./src/bin/d.rs)

--- problems/abc007/src/bin/d.rs
+++ problems/abc007/src/bin/d.rs
@@ -2,20 +2,20 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use proconio::{input, fastout};
-use proconio::marker::*;
-use whiteread::parse_line;
-use std::collections::*;
 use num::*;
 use num_traits::*;
-use superslice::*;
+use proconio::marker::*;
+use proconio::{fastout, input};
+use std::collections::*;
 use std::ops::*;
+use superslice::*;
+use whiteread::parse_line;
 
-use itertools::Itertools;
 use itertools::iproduct;
+use itertools::Itertools;
 use itertools_num::ItertoolsNum;
 
-use competitive::format::*;
+use competitive_internal_mod::format::*;
 
 #[allow(unused_macros)]
 macro_rules! debug {
@@ -25,17 +25,138 @@ macro_rules! debug {
     };
 }
 
+fn digit_dp(n: usize) -> usize {
+    let n_chars = n.to_string().chars().collect_vec();
+    let digit = n.to_string().len();
+
+    /*
+    [上から見た桁数][smaller][4 or 9]
+    遷移は、
+    - smaller = True -> samler = True
+    - smaller = False -> actual d < now_d ? True : False
+    - 4 or 9 = True -> True
+    - 4 or 9 = False -> now_d == 4 or now_d == 9 ? True : False
+    */
+    let mut dp = vec![vec![vec![0; 2]; 2]; digit + 1];
+    dp[0][0][0] = 1;
+    for i in 0..digit {
+        let max_d = n_chars[i] as usize - 48;
+        for smaller in 0..2 {
+            for k in 0..2 {
+                let upper = if smaller == 1 { 9 } else { max_d };
+                for d in 0..=upper {
+                    dp[i + 1][(smaller == 1 || (d < max_d)) as usize]
+                        [(k == 1 || d == 4 || d == 9) as usize] += dp[i][smaller][k];
+                }
+            }
+        }
+    }
+
+    dp[digit][0][1] + dp[digit][1][1]
+}
+
 #[fastout]
 fn solve() -> impl AtCoderFormat {
     const MOD: usize = 1_000_000_007;
     const UINF: usize = std::usize::MAX;
     const IINF: isize = std::isize::MAX;
 
-    input!{}
-    
-    ""
+    input! {
+        a: usize, b: usize,
+    }
+
+    /*
+    桁DP
+    */
+
+    digit_dp(b) - digit_dp(a - 1)
 }
 
 fn main() {
     println!("{}", solve().format());
+}
+
+mod competitive_internal_mod {
+    pub mod format {
+        use std::vec::Vec;
+
+        /// Trait of format for atcoder
+        ///    
+        /// bool -> Yes or No  
+        /// vec![a, b ,c] -> "a\nb\nc"  
+        /// vec![vec![0, 1], vec![1, 0]] -> "0 1\n1 0"  
+        pub trait AtCoderFormat {
+            fn format(&self) -> String;
+        }
+
+        macro_rules! impl_format {
+            ($t: ty) => {
+                impl AtCoderFormat for $t {
+                    fn format(&self) -> String {
+                        self.to_string()
+                    }
+                }
+
+                impl AtCoderFormat for Vec<$t> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+
+                impl AtCoderFormat for Vec<Vec<$t>> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| {
+                                x.iter()
+                                    .map(|x| x.to_string())
+                                    .collect::<Vec<String>>()
+                                    .join(" ")
+                            })
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+            };
+        }
+
+        impl_format!(usize);
+        impl_format!(u128);
+        impl_format!(u64);
+        impl_format!(u32);
+        impl_format!(u16);
+        impl_format!(u8);
+        impl_format!(isize);
+        impl_format!(i128);
+        impl_format!(i64);
+        impl_format!(i32);
+        impl_format!(i16);
+        impl_format!(i8);
+        impl_format!(f32);
+        impl_format!(f64);
+        impl_format!(&str);
+        impl_format!(String);
+        impl_format!(char);
+
+        impl AtCoderFormat for bool {
+            fn format(&self) -> String {
+                if self == &true {
+                    "Yes".to_string()
+                } else {
+                    "No".to_string()
+                }
+            }
+        }
+
+        impl AtCoderFormat for Vec<bool> {
+            fn format(&self) -> String {
+                self.iter()
+                    .map(|x| x.format())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            }
+        }
+    }
 }
