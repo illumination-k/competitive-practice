d917a6cb55246eeb3d0662ab078e7d3393cb4dff
illumination-k <illumination.k.27@gmail.com>
2021-05-09 23:52:10 +0900

    "first commit in abc007"

--- problems/typical90/src/bin/005.rs
+++ problems/typical90/src/bin/005.rs
@@ -2,17 +2,17 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use proconio::{input, fastout};
-use proconio::marker::*;
-use whiteread::parse_line;
-use std::collections::*;
 use num::*;
 use num_traits::*;
-use superslice::*;
+use proconio::marker::*;
+use proconio::{fastout, input};
+use std::collections::*;
 use std::ops::*;
+use superslice::*;
+use whiteread::parse_line;
 
-use itertools::Itertools;
 use itertools::iproduct;
+use itertools::Itertools;
 use itertools_num::ItertoolsNum;
 
 use competitive::format::*;
@@ -31,9 +31,32 @@ fn solve() -> impl AtCoderFormat {
     const UINF: usize = std::usize::MAX;
     const IINF: isize = std::isize::MAX;
 
-    input!{}
-    
-    ""
+    input! {
+        n: usize, b: usize, k: usize,
+        c: [usize; k]
+    }
+
+    /*
+    1. 桁DP
+    https://torus711.hatenablog.com/entry/20150423/1429794075
+    smallerは1 <= ci <= 9なので必要ない
+    2. 行列累乗
+    */
+    let mut dp = vec![vec![0; b]; n + 1];
+    dp[0][0] = 1;
+
+    for i in 0..n {
+        for j in 0..b {
+            // 動ける範囲
+            for &ci in c.iter() {
+                let next = (10 * j + ci) % b;
+                dp[i + 1][next] += dp[i][j];
+                dp[i + 1][next] %= MOD;
+            }
+        }
+    }
+    debug!(dp);
+    dp[n][0]
 }
 
 fn main() {
