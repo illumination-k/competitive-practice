53b708b8fdb557675549f8613106262378583698
illumination-k <illumination.k.27@gmail.com>
2021-05-18 03:00:41 +0900

    rewrite template

--- problems/typical90/src/bin/090.rs
+++ problems/typical90/src/bin/090.rs
@@ -7,7 +7,6 @@ use num_traits::*;
 use proconio::{fastout, input, marker::*};
 use std::{collections::*, ops::*};
 use superslice::*;
-use whiteread::parse_line;
 
 use itertools::{iproduct, Itertools};
 use itertools_num::ItertoolsNum;
@@ -39,4 +38,9 @@ pub mod utils {
         };
     }
     pub(crate) use debug;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }
