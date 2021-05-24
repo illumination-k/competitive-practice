840623cbd8a939f2d973c46886af4bde7c1f5583
illumination-k <illumination.k.27@gmail.com>
2021-05-25 04:33:48 +0900

    autocommit on submission (./src/bin/016.rs)

--- problems/typical90/src/bin/016.rs
+++ problems/typical90/src/bin/016.rs
@@ -12,18 +12,60 @@ use whiteread::parse_line;
 use itertools::{iproduct, Itertools};
 use itertools_num::ItertoolsNum;
 
-use competitive::format::*;
+use competitive_internal_mod::format::*;
 use utils::debug;
 
 const MOD: usize = 1_000_000_007;
 const UINF: usize = std::usize::MAX;
 const IINF: isize = std::isize::MAX;
 
+/*
+DPっぽい
+count(a) + count(b) + count(c) < 10000
+
+普通はdp[j] (j円払ったときの最小枚数)っておくけどn <= 10^9だから厳しい？
+コインは3種類しかない上に数が限られている
+
+コインの最大枚数をkとするとk < 10,000
+
+for x in 0..k {
+    for y in 0..k-x {
+        z = k - x - y;
+        if a * x + y * b + z * c == n {
+            ok
+        }
+    }
+}
+
+*/
+
 #[fastout]
 fn solve() -> impl AtCoderFormat {
-    input! {}
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+    const K: usize = 10000;
+    let mut ans = UINF;
+    for x in 0..K {
+        for y in 0..K - x {
+            let use_val = x * a + y * b;
+            if use_val <= n {
+                let left_val = n - use_val;
+                let z = if left_val % c == 0 {
+                    left_val / c
+                } else {
+                    continue;
+                };
+                // debug!(x, y, z);
+                ans = std::cmp::min(ans, x + y + z);
+            }
+        }
+    }
 
-    ""
+    ans
 }
 
 fn main() {
@@ -39,4 +81,89 @@ pub mod utils {
         };
     }
     pub(crate) use debug;
+}
+
+mod competitive_internal_mod {
+    pub mod format {
+        use std::vec::Vec;
+
+        /// Trait of format for atcoder
+        ///    
+        /// bool -> Yes or No  
+        /// vec![a, b ,c] -> "a\nb\nc"  
+        /// vec![vec![0, 1], vec![1, 0]] -> "0 1\n1 0"  
+        pub trait AtCoderFormat {
+            fn format(&self) -> String;
+        }
+
+        macro_rules! impl_format {
+            ($t: ty) => {
+                impl AtCoderFormat for $t {
+                    fn format(&self) -> String {
+                        self.to_string()
+                    }
+                }
+
+                impl AtCoderFormat for Vec<$t> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+
+                impl AtCoderFormat for Vec<Vec<$t>> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| {
+                                x.iter()
+                                    .map(|x| x.to_string())
+                                    .collect::<Vec<String>>()
+                                    .join(" ")
+                            })
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+            };
+        }
+
+        impl_format!(usize);
+        impl_format!(u128);
+        impl_format!(u64);
+        impl_format!(u32);
+        impl_format!(u16);
+        impl_format!(u8);
+        impl_format!(isize);
+        impl_format!(i128);
+        impl_format!(i64);
+        impl_format!(i32);
+        impl_format!(i16);
+        impl_format!(i8);
+        impl_format!(f32);
+        impl_format!(f64);
+        impl_format!(&str);
+        impl_format!(String);
+        impl_format!(char);
+
+        impl AtCoderFormat for bool {
+            fn format(&self) -> String {
+                if self == &true {
+                    "Yes".to_string()
+                } else {
+                    "No".to_string()
+                }
+            }
+        }
+
+        impl AtCoderFormat for Vec<bool> {
+            fn format(&self) -> String {
+                self.iter()
+                    .map(|x| x.format())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            }
+        }
+    }
 }
