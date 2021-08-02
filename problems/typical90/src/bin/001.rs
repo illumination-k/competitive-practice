6595144e29a7f847a3a98eb7a9ecb935aa16c52a
illumination-k <illumination.k.27@gmail.com>
2021-08-02 14:14:25 +0900

    001.rs

--- problems/typical90/src/bin/001.rs
+++ problems/typical90/src/bin/001.rs
@@ -1,170 +1,97 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 #![allow(dead_code)]
+#![allow(unused_macros)]
 
 use num::*;
 use num_traits::*;
-use proconio::marker::*;
-use proconio::{fastout, input};
-use std::collections::*;
-use std::ops::*;
+use proconio::{fastout, input, marker::*};
+use std::{collections::*, ops::*};
 use superslice::*;
-use whiteread::parse_line;
-
-use itertools::iproduct;
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 use itertools_num::ItertoolsNum;
+use competitive::format::*;
+use utils::*;
 
-use competitive_internal_mod::format::*;
-
-#[allow(unused_macros)]
-macro_rules! debug {
-    ($($a:expr),* $(,)*) => {
-        #[cfg(debug_assertions)]
-        eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
-    };
-}
-
-fn ok(m: usize, k: usize, l: usize, a: &Vec<usize>) -> bool {
-    let mut cnt = 0;
-    let mut pre = 0;
-
-    for &aa in a.iter() {
-        if aa - pre >= m && l - aa >= m {
-            cnt += 1;
-            pre = aa;
-        }
-    }
-
-    cnt >= k
-}
+const MOD: usize = 1_000_000_007;
+const UINF: usize = std::usize::MAX;
+const IINF: isize = std::isize::MAX;
 
 #[fastout]
-fn solve() -> impl AtCoderFormat {
-    const MOD: usize = 1_000_000_007;
-    const UINF: usize = std::usize::MAX;
-    const IINF: isize = std::isize::MAX;
-
-    input! {
-        n: usize, l: usize,
-        k: usize,
-        mut a: [usize; n]
-    }
-
-    a.push(l);
-
-    let mut left = 1;
-    let mut right = l + 1;
-
-    while right - left > 1 {
-        let mid = left + (right - left) / 2;
-
-        debug!(mid, ok(mid, k, l, &a));
-        if !ok(mid, k, l, &a) {
-            right = mid;
-        } else {
-            left = mid;
-        }
-    }
-
-    right - 1
+fn run() -> impl AtCoderFormat {
+    input! {}
+    0
 }
 
 fn main() {
-    println!("{}", solve().format());
+    println!("{}", run().format());
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_ok() {
-        let a = vec![8, 13, 26, 34];
-        debug!(ok(34, 1, 1, &a));
-    }
 }
-mod competitive_internal_mod {
-    pub mod format {
-        use std::vec::Vec;
 
-        /// Trait of format for atcoder
-        ///    
-        /// bool -> Yes or No  
-        /// vec![a, b ,c] -> "a\nb\nc"  
-        /// vec![vec![0, 1], vec![1, 0]] -> "0 1\n1 0"  
-        pub trait AtCoderFormat {
-            fn format(&self) -> String;
-        }
-
-        macro_rules! impl_format {
-            ($t: ty) => {
-                impl AtCoderFormat for $t {
-                    fn format(&self) -> String {
-                        self.to_string()
-                    }
-                }
-
-                impl AtCoderFormat for Vec<$t> {
-                    fn format(&self) -> String {
-                        self.iter()
-                            .map(|x| x.to_string())
-                            .collect::<Vec<String>>()
-                            .join("\n")
-                    }
-                }
-
-                impl AtCoderFormat for Vec<Vec<$t>> {
-                    fn format(&self) -> String {
-                        self.iter()
-                            .map(|x| {
-                                x.iter()
-                                    .map(|x| x.to_string())
-                                    .collect::<Vec<String>>()
-                                    .join(" ")
-                            })
-                            .collect::<Vec<String>>()
-                            .join("\n")
-                    }
-                }
-            };
-        }
-
-        impl_format!(usize);
-        impl_format!(u128);
-        impl_format!(u64);
-        impl_format!(u32);
-        impl_format!(u16);
-        impl_format!(u8);
-        impl_format!(isize);
-        impl_format!(i128);
-        impl_format!(i64);
-        impl_format!(i32);
-        impl_format!(i16);
-        impl_format!(i8);
-        impl_format!(f32);
-        impl_format!(f64);
-        impl_format!(&str);
-        impl_format!(String);
-        impl_format!(char);
-
-        impl AtCoderFormat for bool {
-            fn format(&self) -> String {
-                if self == &true {
-                    "Yes".to_string()
-                } else {
-                    "No".to_string()
-                }
-            }
-        }
-
-        impl AtCoderFormat for Vec<bool> {
-            fn format(&self) -> String {
-                self.iter()
-                    .map(|x| x.format())
-                    .collect::<Vec<String>>()
-                    .join("\n")
-            }
-        }
+pub mod utils {
+    macro_rules! debug {
+        ($($a:expr),* $(,)*) => {
+            #[cfg(debug_assertions)]
+            eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
+        };
     }
+
+    macro_rules! chmin {
+        ($base:expr, $($cmps:expr),+ $(,)*) => {{
+            let cmp_min = min!($($cmps),+);
+            if $base > cmp_min {
+                $base = cmp_min;
+                true;
+            } else {
+                false;
+            }
+        }};
+    }
+
+    macro_rules! chmax {
+        ($base:expr, $($cmps:expr),+ $(,)*) => {{
+            let cmp_max = max!($($cmps),+);
+            if $base < cmp_max {
+                $base = cmp_max;
+                true;
+            } else {
+                false;
+            }
+        }};
+    }
+
+    macro_rules! min {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::min($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::min($a, min!($($rest),+))
+        }};
+    }
+
+    macro_rules! max {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::max($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::max($a, max!($($rest),+))
+        }};
+    }
+
+    pub(crate) use chmax;
+    pub(crate) use chmin;
+    pub(crate) use debug;
+    pub(crate) use max;
+    pub(crate) use min;
 }
+
