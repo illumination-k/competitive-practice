f830eb16fcc14d4c7ab9c5b53ebc5f829e440685
illumination-k <illumination.k.27@gmail.com>
2021-08-02 10:47:59 +0900

    first commit

---
+++ problems/code-festival-2017-quala/src/bin/b.rs
@@ -0,0 +1,96 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_macros)]
+
+use num::*;
+use num_traits::*;
+use proconio::{fastout, input, marker::*};
+use std::{collections::*, ops::*};
+use superslice::*;
+use itertools::{iproduct, Itertools};
+use itertools_num::ItertoolsNum;
+use competitive::format::*;
+use utils::*;
+
+const MOD: usize = 1_000_000_007;
+const UINF: usize = std::usize::MAX;
+const IINF: isize = std::isize::MAX;
+
+#[fastout]
+fn run() -> impl AtCoderFormat {
+    input! {}
+    0
+}
+
+fn main() {
+    println!("{}", run().format());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
+
+pub mod utils {
+    macro_rules! debug {
+        ($($a:expr),* $(,)*) => {
+            #[cfg(debug_assertions)]
+            eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
+        };
+    }
+
+    macro_rules! chmin {
+        ($base:expr, $($cmps:expr),+ $(,)*) => {{
+            let cmp_min = min!($($cmps),+);
+            if $base > cmp_min {
+                $base = cmp_min;
+                true;
+            } else {
+                false;
+            }
+        }};
+    }
+
+    macro_rules! chmax {
+        ($base:expr, $($cmps:expr),+ $(,)*) => {{
+            let cmp_max = max!($($cmps),+);
+            if $base < cmp_max {
+                $base = cmp_max;
+                true;
+            } else {
+                false;
+            }
+        }};
+    }
+
+    macro_rules! min {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::min($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::min($a, min!($($rest),+))
+        }};
+    }
+
+    macro_rules! max {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::max($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::max($a, max!($($rest),+))
+        }};
+    }
+
+    pub(crate) use chmax;
+    pub(crate) use chmin;
+    pub(crate) use debug;
+    pub(crate) use max;
+    pub(crate) use min;
+}
