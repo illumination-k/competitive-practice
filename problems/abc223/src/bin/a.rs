3586910ab3472c6904abed9a6f2f8457916cc13f
illumination-k <illumination.k.27@gmail.com>
2021-10-17 21:01:18 +0900

    AC!! autocommit in abc223-a. link: https://atcoder.jp/contests/abc223/tasks/abc223_a

--- problems/abc223/src/bin/a.rs
+++ problems/abc223/src/bin/a.rs
@@ -3,14 +3,14 @@
 #![allow(dead_code)]
 #![allow(unused_macros)]
 
+use competitive_internal_mod::format::*;
+use itertools::{iproduct, Itertools};
+use itertools_num::ItertoolsNum;
 use num::*;
 use num_traits::*;
 use proconio::{fastout, input, marker::*};
 use std::{collections::*, ops::*};
 use superslice::*;
-use itertools::{iproduct, Itertools};
-use itertools_num::ItertoolsNum;
-use competitive::format::*;
 use utils::*;
 
 const MOD: usize = 1_000_000_007;
@@ -19,8 +19,11 @@ const IINF: isize = std::isize::MAX;
 
 #[fastout]
 fn run() -> impl AtCoderFormat {
-    input! {}
-    0
+    input! {
+        x: usize,
+    }
+
+    x != 0 && x % 100 == 0
 }
 
 fn main() {
@@ -93,4 +96,109 @@ pub mod utils {
     pub(crate) use debug;
     pub(crate) use max;
     pub(crate) use min;
+}
+
+mod competitive_internal_mod {
+    pub mod format {
+        use std::vec::Vec;
+
+        /// Trait of format for atcoder
+        ///    
+        /// bool -> Yes or No  
+        /// vec![a, b ,c] -> "a\nb\nc"  
+        /// vec![vec![0, 1], vec![1, 0]] -> "0 1\n1 0"  
+        pub trait AtCoderFormat {
+            fn format(&self) -> String;
+        }
+
+        macro_rules! impl_format {
+            ($t: ty) => {
+                impl AtCoderFormat for $t {
+                    fn format(&self) -> String {
+                        self.to_string()
+                    }
+                }
+
+                impl AtCoderFormat for Vec<$t> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+
+                impl AtCoderFormat for Vec<Vec<$t>> {
+                    fn format(&self) -> String {
+                        self.iter()
+                            .map(|x| {
+                                x.iter()
+                                    .map(|x| x.to_string())
+                                    .collect::<Vec<String>>()
+                                    .join(" ")
+                            })
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    }
+                }
+            };
+        }
+
+        impl_format!(usize);
+        impl_format!(u128);
+        impl_format!(u64);
+        impl_format!(u32);
+        impl_format!(u16);
+        impl_format!(u8);
+        impl_format!(isize);
+        impl_format!(i128);
+        impl_format!(i64);
+        impl_format!(i32);
+        impl_format!(i16);
+        impl_format!(i8);
+        impl_format!(f32);
+        impl_format!(f64);
+        impl_format!(&str);
+        impl_format!(String);
+
+        impl AtCoderFormat for char {
+            fn format(&self) -> String {
+                self.to_string()
+            }
+        }
+
+        impl AtCoderFormat for Vec<char> {
+            fn format(&self) -> String {
+                self.iter().collect::<String>()
+            }
+        }
+
+        impl AtCoderFormat for Vec<Vec<char>> {
+            fn format(&self) -> String {
+                self.iter()
+                    .map(|v| v.format())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            }
+        }
+
+        impl AtCoderFormat for bool {
+            fn format(&self) -> String {
+                if self == &true {
+                    "Yes".to_string()
+                } else {
+                    "No".to_string()
+                }
+            }
+        }
+
+        impl AtCoderFormat for Vec<bool> {
+            fn format(&self) -> String {
+                self.iter()
+                    .map(|x| x.format())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            }
+        }
+    }
 }
