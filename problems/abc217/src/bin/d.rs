1e5df8db68191da5d7d041ddd4f3d2cc6ec82bca
illumination-k <illumination.k.27@gmail.com>
2021-12-20 00:48:56 +0900

    "first commit in abc217"

--- problems/abc217/src/bin/d.rs
+++ problems/abc217/src/bin/d.rs
@@ -12,7 +12,7 @@ use proconio::{fastout, input, marker::*, source::auto::AutoSource};
 use std::{
     collections::*,
     io::{BufRead, BufReader},
-    ops::*, cmp::Reverse,
+    ops::*,
 };
 use superslice::*;
 use utils::*;
@@ -21,44 +21,11 @@ const MOD: usize = 1_000_000_007;
 const UINF: usize = std::usize::MAX;
 const IINF: isize = std::isize::MAX;
 
-fn get_coin(val: usize, coins: &[usize]) -> (usize, Option<usize>) {
-    let i = coins.lower_bound(&val);
-
-    if i >= coins.len() {
-        (coins[i], None)
-    } else {
-        if val == coins[i] {
-            (coins[i], None)
-        } else {    
-            (coins[i], Some(coins[i+1]))
-        }
-    }
-}
-
-fn exact_coin_num(mut val: usize, coins: &[usize]) -> usize {
-    let mut sum = 0;
-    let mut lower_index = coins.upper_bound(&val);
-    while lower_index != 0 && val != 0 {
-        debug!(lower_index, val);
-        let coin_num = val / coins[lower_index];
-        val -= coins[lower_index] * coin_num;
-        sum += coin_num;
-        lower_index = coins.upper_bound(&val);
-    }
-    sum
-}
-
-
 #[fastout]
 fn run<R: BufRead>(mut source: AutoSource<R>) -> impl AtCoderFormat {
     input! {
         from &mut source,
-        n: usize, x: usize,
-        coins: [usize; n]
     }
-
-    let mut bq: BinaryHeap<Reverse<usize>> = BinaryHeap::new();
-
     0
 }
 
@@ -71,28 +38,11 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use std::vec;
-
     use super::*;
     use competitive::test_utility::*;
-
-    #[test]
-    fn test_get_exact_num() {
-        let coins = vec![2, 4, 8, 16];
-        dbg!(exact_coin_num(12, &coins));
-    }
 }
 
 pub mod utils {
-    use num_traits::PrimInt;
-    pub fn ceil<T>(lhs: T, rhs: T) -> T 
-        where
-            T: PrimInt
-    {
-        let one = rhs / rhs;
-        (lhs + rhs - one) / rhs
-    }
-    
     macro_rules! debug {
         ($($a:expr),* $(,)*) => {
             #[cfg(debug_assertions)]
@@ -147,8 +97,6 @@ pub mod utils {
             std::cmp::max($a, max!($($rest),+))
         }};
     }
-
-    use std::process::Output;
 
     pub(crate) use chmax;
     pub(crate) use chmin;
